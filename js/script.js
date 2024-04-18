35f62606e044947388fa7379ee538032846a52dc
SMILE-NM <123355875+SMILE-NM@users.noreply.github.com>
2024-04-18 09:25:21 +0500

    modal fully

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 window.addEventListener('DOMContentLoaded', () => {
   const tabs = document.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent'),
@@ -38,7 +39,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Timer
 
-  const deadline = '2023-04-17';
+  const deadline = '2024-05-17';
 
   function getTimeRemaining(endtime) {
     let days, hours, minutes, seconds;
@@ -92,4 +93,55 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   setClock('.timer', deadline);
+
+  //Modal
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]');
+
+  const modalOpenByTime = setTimeout(openModal, 5000);
+
+  modalTrigger.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      openModal();
+    });
+  });
+
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    clearTimeout(modalOpenByTime);
+  }
+
+  function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  }
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) closeModal();
+  });
+
+  function showModalByScroll() {
+    if (
+      window.scrollY + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
 });
