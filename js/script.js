aea756edd128c2d18b13c4d0ba046b8278fe14c2
SMILE-NM <123355875+SMILE-NM@users.noreply.github.com>
2024-04-22 09:04:14 +0500

    Send POST request created

--- js/script.js
+++ js/script.js
@@ -166,7 +166,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     render() {
       const element = document.createElement('div');
-      console.log(this.classes);
       this.classes.forEach((className) => element.classList.add(className));
 
       element.innerHTML = `
@@ -210,4 +209,61 @@ window.addEventListener('DOMContentLoaded', () => {
     21,
     '.menu .container',
   ).render();
+
+  //Form
+
+  const forms = document.querySelectorAll('form');
+  const message = {
+    loading: 'Loading...',
+    success: 'Thanks! We will contact you',
+    failure: 'Something went wrong',
+  };
+
+  forms.forEach((item) => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      let statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+
+      request.open('POST', 'server.php');
+
+      // request.setRequestHeader('Content-type', 'multipart/form-data');
+      // When we use (new FormData with XMLHttpRequest) we should not use SetHeader
+
+      request.setRequestHeader('Content-type', 'application/json');
+
+      const formData = new FormData(form);
+      const obj = {};
+
+      formData.forEach((value, key) => {
+        obj[key] = value;
+      });
+
+      const json = JSON.stringify(obj);
+      request.send(json);
+
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 3000);
+        } else {
+          statusMessage.textContent = message.failure;
+          console.log(request.status);
+        }
+      });
+    });
+  }
 });
